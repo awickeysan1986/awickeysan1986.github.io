745b2d391016b59aa36a5d26cb7ee5f403ea6547
awickeysan1986 <a.wickeysan1986@gmail.com>
2026-06-04 14:25:48 +0900

    2nd Commit!!

---
+++ BAK/index .js
@@ -0,0 +1,66 @@
+//変数
+//let unko = "I am Jun";
+
+//表示
+//console. log(unko);
+
+//定数
+//const bigUnko = "He..Hello World";
+
+//配列
+//let inoki = ["いーち","にー","さーん","だーー"];
+//console.log(inoki); //全部表示
+//console.log(inoki[2]); //3番目だけ表示（0からカウント）
+
+//ループ
+//let index = 0;
+//while(index < inoki.length) { console.log(index); //inoki.lengthで4をゲットする
+//index ++; //1足す（これないと無限ループになる）
+//}
+// if_else
+//if(inoki.length >5){
+//console.log ("ボンバイエ");
+//} else {
+//console.log ("ボンバ。。。");
+//}
+
+//関数
+//const test = (arg) => {
+//if(inoki.length > arg){
+//console.log ("ボンバイエ");
+// } else {
+//console.log ("ボンバ。。。");
+// }
+//};
+//test (3);//この値がargに入る
+//test (4);
+//test (5);//値を変えて何回も実行できる
+
+
+//オブジェクト
+//const unko2 = {
+    //color: "pink",
+    //size: "large",
+    //purfume: "mint",
+    //goToilet: ()=> {
+      //  console.log("Hello World!")
+    //}
+//};
+//console.log (unko2.goToilet());//unko2の中のgoToilet関数を表示する
+//console.log(window.innerHeight); //ウインドウの横サイズを表示する
+//window.alert("ご注意ください");//ポップアップアラート"ご注意ください"を表示する
+//console.log(document.getElementsByTagName("button")[0]);//htmlファイルを参照したい場合の取得
+
+//document.getElementsByTagName("button")[0].addEventListener("click", ()=> {
+    //命令を書く
+  //  window.alert("1つ目のボタンを押しました");
+//});
+
+
+
+
+
+
+
+
+
